11102cd32c071e9ce1138c640e6722a037fe4b49
shaheertiger <shaheertiger@users.noreply.github.com>
2026-08-15 10:34:43 -0700

    Apply Cormorant Garamond typography hierarchy globally and remove font preview route

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Home from './pages/Home';
 import Therapies from './pages/Therapies';
 import AboutUs from './pages/AboutUs';
 import Contact from './pages/Contact';
-import TeamFontPreview from './pages/TeamFontPreview';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -51,7 +50,6 @@ function App() {
             <Route path="/therapies" element={<Therapies />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/team-preview" element={<TeamFontPreview />} />
           </Routes>
         </div>
         <Footer />
